2b0f9f1caddbcf1a0f2c176c00120e912d666791
Soha Jin <soha@lohu.info>
2018-02-09 14:14:51 +0800

    Add project files.

---
+++ FRC Radio Utility Language Fixer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FRC_Radio_Utility_Language_Fixer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
